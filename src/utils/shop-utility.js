e6457dc28f3dc1f253bb1d147c80a352e74c8634
Riasadul Hoq <shaon777@gmail.com>
2025-11-07 20:33:34 +0000

    Image paths updated

--- src/utils/shop-utility.js
+++ src/utils/shop-utility.js
@@ -1,5 +1,5 @@
 export const getImageUrl = (fileName) => {
-  const url = `../assets/img/${fileName}`;
+  const url = `/img/${fileName}`;
   const base = import.meta.url;
   return new URL(url, base).href;
 };
